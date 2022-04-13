d577ad1c21cd13f2227b830a201646340f3d5683
Akshit Kumar Chandora <91782956+axitchandora@users.noreply.github.com>
2022-04-13 15:12:34 +0530

    Time: 0 ms (100.00%), Space: 44 MB (81.80%) - LeetHub

---
+++ 141-linked-list-cycle/141-linked-list-cycle.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        //Using Fast and Slow Pointer Approach
+        ListNode fast=head;
+        ListNode slow=head;
+        while(fast!=null && fast.next!=null){
+            fast=fast.next.next;
+            slow=slow.next;
+            if(slow == fast){
+                return true;
+            }
+        }
+        return false;
+        //Time Complexity - O(N)
+        //Space Compexity - O(1)
+    }
+}
