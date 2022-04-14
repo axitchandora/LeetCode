e3bf02f54fa6baf1c4fb31ee31cc5f121c6ebaae
Akshit Kumar Chandora <91782956+axitchandora@users.noreply.github.com>
2022-04-14 12:19:12 +0530

    Time: 775 ms (19.89%), Space: 0B (100.00%) - LeetHub

--- 1890-the-latest-login-in-2020/1890-the-latest-login-in-2020.sql
+++ 1890-the-latest-login-in-2020/1890-the-latest-login-in-2020.sql
@@ -1,4 +1,4 @@
-# Using DATE_FORMAT
+# Using LIKE
 SELECT DISTINCT user_id,MAX(time_stamp) AS last_stamp FROM Logins
-WHERE DATE_FORMAT(time_stamp,"%Y")=2020
+WHERE time_stamp LIKE "2020%"
 GROUP BY user_id;
