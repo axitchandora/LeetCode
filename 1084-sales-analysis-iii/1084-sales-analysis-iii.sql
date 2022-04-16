36a87f1034a5e6ff87c1a0270a22711400c1f18e
Akshit Kumar Chandora <91782956+axitchandora@users.noreply.github.com>
2022-04-17 00:03:56 +0530

    Time: 1628 ms (9.10%), Space: 0B (100.00%) - LeetHub

--- 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
+++ 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
@@ -4,4 +4,4 @@ FROM Product p
 WHERE product_id NOT IN(
     SELECT product_id FROM sales
     WHERE sale_date <'2019-01-01' OR sale_date >'2019-03-31'
-);
+) AND product_id IN(SELECT product_id FROM sales);
