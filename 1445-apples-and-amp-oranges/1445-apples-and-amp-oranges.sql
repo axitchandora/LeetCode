17723b2cd40d9846dd4ac9582173c682c3cfa471
Akshit Kumar Chandora <91782956+axitchandora@users.noreply.github.com>
2022-04-21 22:22:57 +0530

    Time: 308 ms (62.94%), Space: 0B (100.00%) - LeetHub

--- 1445-apples-and-amp-oranges/1445-apples-and-amp-oranges.sql
+++ 1445-apples-and-amp-oranges/1445-apples-and-amp-oranges.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
-SELECT sale_date, SUM(sold_num*IF(fruit='oranges',-1,1))AS diff
+SELECT sale_date, SUM(sold_num* CASE WHEN fruit='oranges' THEN -1 ELSE 1 END) AS diff
 FROM Sales
 GROUP BY sale_date;
