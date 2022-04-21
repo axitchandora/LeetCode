0c9a97ef2f42699d1721b22f26033d5d6e992c3c
Akshit Kumar Chandora <91782956+axitchandora@users.noreply.github.com>
2022-04-21 21:58:04 +0530

    Time: 659 ms (42.15%), Space: 0B (100.00%) - LeetHub

---
+++ 1699-number-of-calls-between-two-persons/1699-number-of-calls-between-two-persons.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+SELECT from_id  AS person1,to_id AS person2, COUNT(*) AS call_count,
+SUM(duration) AS total_duration
+FROM Calls
+GROUP BY LEAST(from_id,to_id),GREATEST(from_id,to_id);
