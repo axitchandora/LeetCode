e0b881c05cf67be452d4adf12e3effe2d3cff247
Akshit Kumar Chandora <91782956+axitchandora@users.noreply.github.com>
2022-04-26 21:21:40 +0530

    Time: 341 ms (32.29%), Space: 0B (100.00%) - LeetHub

---
+++ 626-exchange-seats/626-exchange-seats.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+SELECT(CASE WHEN MOD(id, 2) != 0 AND counts != id THEN id + 1
+WHEN MOD(id, 2) != 0 AND counts = id THEN id
+ELSE id - 1 END) AS id, student
+FROM seat,(SELECT COUNT(*) AS counts FROM seat) AS seat_counts
+ORDER BY id ASC;
