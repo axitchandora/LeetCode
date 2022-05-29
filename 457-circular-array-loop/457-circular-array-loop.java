749abd4ccb42260600fe9def5708a993a50d74bc
Akshit Kumar Chandora <91782956+axitchandora@users.noreply.github.com>
2022-05-30 00:46:20 +0530

    Time: 46 ms (12.95%), Space: 41.4 MB (60.79%) - LeetHub

--- 457-circular-array-loop/457-circular-array-loop.java
+++ 457-circular-array-loop/457-circular-array-loop.java
@@ -1,37 +1,31 @@
 class Solution {
     public boolean circularArrayLoop(int[] nums) {
-    for (int i = 0; i < nums.length; i++) {
-      boolean isForward = nums[i] >= 0; // if we are moving forward or not
-      int slow = i, fast = i;
-
-      // if slow or fast becomes '-1' this means we can't find cycle for this number
-      do {
-        slow = findNextIndex(nums, isForward, slow); // move one step for slow pointer
-        fast = findNextIndex(nums, isForward, fast); // move one step for fast pointer
-        if (fast != -1)
-          fast = findNextIndex(nums, isForward, fast); // move another step for fast pointer
-      } while (slow != -1 && fast != -1 && slow != fast);
-
-      if (slow != -1 && slow == fast)
-        return true;
+        for(int i=0;i<nums.length;i++){
+            int slow=i;
+            int fast=i;
+            float direction=Math.signum(nums[i]);            
+            do{
+                slow=getNextIndex(nums,direction,slow);
+                fast=getNextIndex(nums,direction,fast);
+                if(fast!=-1)
+                    fast=getNextIndex(nums,direction,fast);
+                if(fast==-1 || nums[slow]==0 || nums[fast]==0)
+                    break;                
+            }while(slow!=fast);
+            
+            if(slow!=-1 && slow==fast)
+                return true;
+            nums[i]=0;
+        }
+        return false;
     }
-
-    return false;
-  }
-
-  private static int findNextIndex(int[] arr, boolean isForward, int currentIndex) {
-    boolean direction = arr[currentIndex] >= 0;
-    if (isForward != direction)
-      return -1; // change in direction, return -1
-
-    int nextIndex = (currentIndex + arr[currentIndex]) % arr.length;
-    if (nextIndex < 0)
-      nextIndex += arr.length; // wrap around for negative numbers
-
-    // one element cycle, return -1 
-    if (nextIndex == currentIndex)
-      nextIndex = -1;
-
-    return nextIndex;
-  }
+    private int getNextIndex(int[] nums,float direction,int currIndex){
+        float currDirection=Math.signum(nums[currIndex]);
+        if(currDirection*direction<0)
+            return -1;
+        int index=(currIndex+nums[currIndex])%nums.length;
+        if(index<0)
+            index+=nums.length;
+        return index==currIndex?-1:index;
+    }
 }
