4b021d77fb1eb169766f7c703cb858b874935313
Akshit Kumar Chandora <91782956+axitchandora@users.noreply.github.com>
2022-05-30 00:49:25 +0530

    Time: 26 ms (36.59%), Space: 39.7 MB (89.87%) - LeetHub

--- 457-circular-array-loop/457-circular-array-loop.java
+++ 457-circular-array-loop/457-circular-array-loop.java
@@ -1,21 +1,22 @@
 class Solution {
-    public boolean circularArrayLoop(int[] nums) {
-        for(int i=0;i<nums.length;i++){
-            int slow=i;
-            int fast=i;
-            float direction=Math.signum(nums[i]);            
-            do{
-                slow=getNextIndex(nums,direction,slow);
-                fast=getNextIndex(nums,direction,fast);
-                if(fast!=-1)
-                    fast=getNextIndex(nums,direction,fast);
-                if(fast==-1 || nums[slow]==0 || nums[fast]==0)
-                    break;                
-            }while(slow!=fast);
-            
-            if(slow!=-1 && slow==fast)
+public boolean circularArrayLoop(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            float direction = Math.signum(nums[i]);
+            int slow = i;
+            int fast = i;
+
+            do {
+                slow = getNextIndex(nums, direction, slow);
+                fast = getNextIndex(nums, direction, fast);
+                if (fast != -1)
+                    fast = getNextIndex(nums, direction, fast);
+                if (fast == -1 || nums[slow] == 0 || nums[fast] == 0)
+                    break;
+            } while (slow != fast);
+
+            if (slow != -1 && slow == fast) 
                 return true;
-            nums[i]=0;
+            nums[i] = 0;
         }
         return false;
     }
