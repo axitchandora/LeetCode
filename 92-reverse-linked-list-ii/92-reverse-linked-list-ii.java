f7500f60b7d156c022c7f9244279998664ae5327
Akshit Kumar Chandora <91782956+axitchandora@users.noreply.github.com>
2022-06-05 01:57:11 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (35.52%) - LeetHub

--- 92-reverse-linked-list-ii/92-reverse-linked-list-ii.java
+++ 92-reverse-linked-list-ii/92-reverse-linked-list-ii.java
@@ -15,26 +15,34 @@ class Solution {
         // after skipping 'p-1' nodes, current will point to 'p'th node
         ListNode current = head, previous = null;
         
-        for(int i = 0; current != null && i < left - 1; ++i) {
+        for(int i = 0; current != null && i < left - 1; i++) {
             previous = current;
             current = current.next;
         }
         
-        ListNode lastNodeOfFirstPart=previous;
-        
-        ListNode lastNodeOfSubPart=current;
-        ListNode next=null;
-        for(int i=0;current!=null && i< right-left+1;i++){
-            next=current.next;
-            current.next=previous;
-            previous=current;
-            current=next;
+        // we are interested in three parts of the LinkedList, part before index 'p', part between 'p' and 
+        // 'q', and the part after index 'q'
+        ListNode lastNodeOfFirstPart = previous; // points to the node at index 'p-1'
+        // after reversing the LinkedList 'current' will become the last node of the sub-list
+        ListNode lastNodeOfSubList = current;
+        ListNode next = null; // will be used to temporarily store the next node
+        // reverse nodes between 'p' and 'q'
+        for (int i = 0; current != null && i < right - left + 1; i++) {
+            next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
         }
-        if(lastNodeOfFirstPart!=null)
-            lastNodeOfFirstPart.next=previous;
-        else
-            head=previous;
-        lastNodeOfSubPart.next=current;
+
+        // connect with the first part
+        if (lastNodeOfFirstPart != null)
+            lastNodeOfFirstPart.next = previous; // 'previous' is now the first node of the sub-list
+        else // this means p == 1 i.e., we are changing the first node (head) of the LinkedList
+            head = previous;
+
+        // connect with the last part
+        lastNodeOfSubList.next = current;
+
         return head;
     }
 }
