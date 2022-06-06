b9f66c92663cbff762616ce78912bfed18c6f972
Akshit Kumar Chandora <91782956+axitchandora@users.noreply.github.com>
2022-06-07 01:32:07 +0530

    Time: 15 ms (34.44%), Space: 255.9 MB (61.11%) - LeetHub

--- 2074-reverse-nodes-in-even-length-groups/2074-reverse-nodes-in-even-length-groups.java
+++ 2074-reverse-nodes-in-even-length-groups/2074-reverse-nodes-in-even-length-groups.java
@@ -11,7 +11,7 @@
 class Solution {
     public ListNode reverseEvenLengthGroups(ListNode head) {
         if(head==null || head.next==null) return head;
-        
+        // find the length and the last node of the list
         int lengthList=0;
         ListNode tmp=head;
         while(tmp!=null){
@@ -21,10 +21,15 @@ class Solution {
         ListNode current=head,previous=null;
         int k=1;
         while(current!=null){
+            // If k is Even and k <= Remaining Length of LinkedList OR
+            // (Remaining Length of LinkedList) is even and k is greater than (Remaining Length of LinkedList)
+            // we will reverse the kth Sub-Linked List
+            // Else we will skip the k elements in Linked List
             if((k%2==0 && k<=lengthList)|| (k>lengthList && lengthList%2==0)){
-                ListNode lastNodeOfPreviousPart=previous;            
+                ListNode lastNodeOfPreviousPart=previous; 
+                // after reversing the LinkedList 'current' will become the last node of the sub-list
                 ListNode lastNodeOfSubList=current;
-                ListNode next=null;
+                ListNode next=null; // will be used to temporarily store the next node
                 for(int i=0;i<k && current!=null;i++){
                     next=current.next;
                     current.next=previous;
@@ -32,22 +37,25 @@ class Solution {
                     current=next;
                     lengthList--;
                 }
+                 // connect with the previous part
                 if(lastNodeOfPreviousPart!=null)
-                    lastNodeOfPreviousPart.next=previous;
-                else
+                    lastNodeOfPreviousPart.next=previous; // 'previous' is now the first node of the sub-list
+                else // this means we are changing the first node (head) of the LinkedList
                     head=previous;
-                k++;
+                // connect with the next part
                 lastNodeOfSubList.next=current;
+                // prepare for the next sub-list
                 previous=lastNodeOfSubList;
             }else{
+                // Skiping the k elements in LinkedList
                 for(int i=0;i<k && current!=null;i++){
                     previous=current;
                     current=current.next;
                     lengthList--;
                 }
-                k++;
             }
-            
+            // incrementing the k 
+            k++;            
         }
         return head;
     }
