cd78d3cc63305f1f18d9b41f9e86bf78d7b7e678
Akshit Kumar Chandora <91782956+axitchandora@users.noreply.github.com>
2022-06-16 20:47:32 +0530

    Time: 1 ms (99.04%), Space: 47.9 MB (43.03%) - LeetHub

--- 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.java
+++ 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.java
@@ -14,19 +14,30 @@
  * }
  */
 class Solution {
-    private int answer;
+    private int globalMaximumSum;
     public int maxPathSum(TreeNode root) {
-        if(root==null) return 0;
-        answer=Integer.MIN_VALUE;
-        findMaximumPathSum(root);
-        return answer;
+        globalMaximumSum=Integer.MIN_VALUE;
+        findMaximumPathSumRecursive(root);
+        return globalMaximumSum;
     }
-    private int findMaximumPathSum(TreeNode root){
-        if(root==null)return 0;
-        int leftMax=Math.max(findMaximumPathSum(root.left),0);
-        int rightMax=Math.max(findMaximumPathSum(root.right),0);        
-        int currSum=leftMax+rightMax+root.val;        
-        answer=Math.max(answer,currSum);        
-        return root.val+Math.max(leftMax,rightMax);        
+    private int findMaximumPathSumRecursive(TreeNode currentNode){
+        if(currentNode==null) return 0;
+        
+        int maxPathSumFromLeft=findMaximumPathSumRecursive(currentNode.left);
+        int maxPathSumFromRight=findMaximumPathSumRecursive(currentNode.right);
+        // ignore paths with negative sums, since we need to find the maximum sum we should
+        // ignore any path which has an overall negative sum.
+        maxPathSumFromLeft=Math.max(maxPathSumFromLeft,0);
+        maxPathSumFromRight=Math.max(maxPathSumFromRight,0);
+        // maximum path sum at the current node will be equal to the sum from the left subtree +
+        // the sum from right subtree + val of current node
+        int currentMaximumSum=maxPathSumFromLeft + maxPathSumFromRight + currentNode.val;
+        
+        // update the global maximum sum
+        globalMaximumSum=Math.max(globalMaximumSum,currentMaximumSum);
+        // maximum sum of any path from the current node will be equal to the maximum of 
+        // the sums from left or right subtrees plus the value of the current node
+        return Math.max(maxPathSumFromLeft, maxPathSumFromRight) + currentNode.val;
     }
+    
 }
