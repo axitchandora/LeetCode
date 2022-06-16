8d2630482a9aac3282dd93c642c69cf2adabb521
Akshit Kumar Chandora <91782956+axitchandora@users.noreply.github.com>
2022-06-16 13:15:27 +0530

    Time: 3 ms (53.22%), Space: 42.8 MB (11.38%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.java
+++ 389-find-the-difference/389-find-the-difference.java
@@ -1,18 +1,10 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-        int n1=s.length();
-        int n2=t.length();
-        int [] freqCountOfS=new int[256];
-        int [] freqCountOfT=new int[256];
-        char result='a';
-        for(int i=0;i<n1;i++)
-            freqCountOfS[s.charAt(i)]++;
-        for(int i=0;i<n2;i++)
-            freqCountOfT[t.charAt(i)]++;
-        for(int i=0;i<256;i++)
-            if(freqCountOfS[i]!=freqCountOfT[i]){
-                result=(char)i;
-            }
+        char result=0;
+        for(int i=0;i<s.length();i++)
+            result^=s.charAt(i);
+        for(int i=0;i<t.length();i++)
+            result^=t.charAt(i);
         return result;
     }
 }
