f7409b7b8894fc39ba8d85fd773b40178f2ff11f
Akshit Kumar Chandora <91782956+axitchandora@users.noreply.github.com>
2022-06-17 20:03:45 +0530

    Time: 0 ms (100.00%), Space: 42.4 MB (38.40%) - LeetHub

--- 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.java
+++ 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.java
@@ -5,7 +5,7 @@ class Solution {
         int n=s.length();
         for(int i=0;i<n;i++){
             if(i<n-2 && charArray[i+2]=='#'){
-                result.append((char)((charArray[i]-'0')*10+(charArray[i+1]-'0')+'a'-1));
+                result.append((char)((charArray[i]-'0')*10+(charArray[i+1]-'1')+'a'));
                 i+=2;
             }else
                 result.append((char)((charArray[i]-'1')+'a'));
