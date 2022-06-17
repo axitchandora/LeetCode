5b00bb598c80224b32a23d41aecab7c93c9eee09
Akshit Kumar Chandora <91782956+axitchandora@users.noreply.github.com>
2022-06-17 20:03:21 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (95.91%) - LeetHub

--- 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.java
+++ 1309-decrypt-string-from-alphabet-to-integer-mapping/1309-decrypt-string-from-alphabet-to-integer-mapping.java
@@ -1,13 +1,14 @@
 class Solution {
     public String freqAlphabets(String s) {        
         StringBuilder result=new StringBuilder();
+        char[] charArray=s.toCharArray();
         int n=s.length();
         for(int i=0;i<n;i++){
-            if(i<n-2 && s.charAt(i+2)=='#'){
-                result.append((char)((s.charAt(i)-'0')*10+(s.charAt(i+1)-'0')+'a'-1));
+            if(i<n-2 && charArray[i+2]=='#'){
+                result.append((char)((charArray[i]-'0')*10+(charArray[i+1]-'0')+'a'-1));
                 i+=2;
             }else
-                result.append((char)((s.charAt(i)-'1')+'a'));
+                result.append((char)((charArray[i]-'1')+'a'));
         }
         return result.toString();
     }
