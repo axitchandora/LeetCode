ceac22842b486cf8e1b164f1ef86bea18bdd989a
Akshit Kumar Chandora <91782956+axitchandora@users.noreply.github.com>
2022-06-21 19:48:10 +0530

    Time: 3 ms (21.07%), Space: 81.8 MB (10.08%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.java
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -1,17 +1,14 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int currSum=0;
         int maxSum=Integer.MIN_VALUE;
-        for(int i=0;i<nums.length;i++){
-            currSum+=nums[i];
+        int currSum=0;
+        for(int i:nums){
+            currSum+=i;
             maxSum=Math.max(maxSum,currSum);
             if(currSum<0){
                 currSum=0;
             }
         }
-        return maxSum;  
-        // Using Kadane's algorithm
-        // Time Complexity - Big O(N)
-        // Space Complexity - Big O(1)
+        return maxSum;
     }
 }
