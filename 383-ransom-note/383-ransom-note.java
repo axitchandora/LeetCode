c407d797812f6eb7ab2663da7f287d792c6c79b8
Akshit Kumar Chandora <91782956+axitchandora@users.noreply.github.com>
2022-06-27 11:30:10 +0530

    Time: 3 ms (90.18%), Space: 46.8 MB (43.03%) - LeetHub

--- 383-ransom-note/383-ransom-note.java
+++ 383-ransom-note/383-ransom-note.java
@@ -1,16 +1,13 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        int[] freqMapOfRansomNote=new int[26];
         int[] freqMapOfMagazine=new int[26];
-        for(char ch:ransomNote.toCharArray()){
-            freqMapOfRansomNote[ch-'a']++;
-        }
         for(char ch:magazine.toCharArray()){
             freqMapOfMagazine[ch-'a']++;
         }
-        for(int i=0;i<26;i++)
-            if(freqMapOfRansomNote[i]>freqMapOfMagazine[i])
-                return false;
+        for(char ch:ransomNote.toCharArray()){
+            if(freqMapOfMagazine[ch-'a']==0) return false;
+            freqMapOfMagazine[ch-'a']--;
+        }
         return true;
     }
 }
