2410469b684db3086b34b44b61b4ec22a6c6140f
Akshit Kumar Chandora <91782956+axitchandora@users.noreply.github.com>
2022-06-30 22:36:15 +0530

    Time: 2 ms (74.62%), Space: 41.4 MB (54.90%) - LeetHub

---
+++ 7-reverse-integer/7-reverse-integer.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int reverse(int x) {
+        long reverse=0;
+        boolean isNegative=false;
+        if(x<0) isNegative=true;
+        int num=Math.abs(x);
+        while(num>0){
+            int tmp=num%10;
+            reverse=reverse*10+tmp;
+            num/=10;
+        }
+        if(reverse>=Integer.MAX_VALUE) return 0;
+        if(isNegative) return -1*(int)reverse;
+        else return (int)reverse;
+    }
+}
