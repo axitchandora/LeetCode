7c771d08edc2ce02ca978370bd4259c5096275b1
Akshit Kumar Chandora <91782956+axitchandora@users.noreply.github.com>
2022-07-06 00:50:49 +0530

    Time: 3 ms (80.10%), Space: 74.3 MB (24.08%) - LeetHub

--- 997-find-the-town-judge/997-find-the-town-judge.java
+++ 997-find-the-town-judge/997-find-the-town-judge.java
@@ -1,9 +1,9 @@
 class Solution {
     public int findJudge(int n, int[][] trust) {
         int[] diffOfInAndOutDegree=new int[n+1];
-        for(int[] t:trust){
-            diffOfInAndOutDegree[t[0]]--;
-            diffOfInAndOutDegree[t[1]]++;
+        for(int t=0;t<trust.length;t++){
+            diffOfInAndOutDegree[trust[t][0]]--;
+            diffOfInAndOutDegree[trust[t][1]]++;
         }
         for(int i=1;i<=n;i++){
             if(diffOfInAndOutDegree[i]==n-1)
