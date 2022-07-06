07328414d42acc8ef9489f13e8b9fc659f2c7335
Akshit Kumar Chandora <91782956+axitchandora@users.noreply.github.com>
2022-07-07 00:18:25 +0530

    Time: 6 ms (76.27%), Space: 65.1 MB (73.77%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -13,15 +13,17 @@ class Solution {
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]==1)
-                    noOfLand+=dfs(grid,i,j);
+                    noOfLand++;
             }
         }
         return noOfLand;
     }
-    private int dfs(int[][] grid,int i,int j){
-        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length ||  grid[i][j]==0) return 0;
-        
+    private void dfs(int[][] grid,int i,int j){
+        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length ||  grid[i][j]==0) return;        
         grid[i][j]=0;
-        return 1+dfs(grid,i-1,j)+dfs(grid,i,j+1)+dfs(grid,i,j-1)+dfs(grid,i+1,j);
+        dfs(grid,i-1,j);
+        dfs(grid,i,j+1);
+        dfs(grid,i,j-1);
+        dfs(grid,i+1,j);
     }
 }
