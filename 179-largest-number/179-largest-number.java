c4079d720d253e4c4879deb8dbcf0546c59f46bc
Akshit Kumar Chandora <91782956+axitchandora@users.noreply.github.com>
2022-07-07 22:17:49 +0530

    Time: 12 ms (34.94%), Space: 43.8 MB (49.17%) - LeetHub

--- 179-largest-number/179-largest-number.java
+++ 179-largest-number/179-largest-number.java
@@ -1,18 +1,16 @@
 class Solution {
     public String largestNumber(int[] nums) {
-        Integer[] boxedNums=new Integer[nums.length];
-        for(int i=0;i<nums.length;i++){
-            boxedNums[i]=nums[i];
-        }
-        Arrays.sort(boxedNums,(a,b)->{
+        String[] strNums = new String[nums.length];
+        for (int i=0; i<nums.length; i++) strNums[i] = Integer.toString(nums[i]);
+        Arrays.sort(strNums,(a,b)->{
             String order1=""+a+b;
             String order2=""+b+a;
             return order2.compareTo(order1);
         });
-        if(boxedNums[0]==0) return "0";
+        if(strNums[0].equals("0")) return "0";
         StringBuilder sb=new StringBuilder();
-        for(int i=0;i<boxedNums.length;i++)
-            sb.append(boxedNums[i]);
+        for(int i=0;i<strNums.length;i++)
+            sb.append(strNums[i]);
         return sb.toString();
     }
 }
