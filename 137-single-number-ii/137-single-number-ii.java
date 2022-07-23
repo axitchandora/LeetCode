09f040f0c2a1bd25625ba8a67d3c7eee259e276b
Akshit Kumar Chandora <91782956+axitchandora@users.noreply.github.com>
2022-07-24 02:53:16 +0530

    Time: 1 ms (94.58%), Space: 43.9 MB (65.26%) - LeetHub

---
+++ 137-single-number-ii/137-single-number-ii.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int ones=0;
+        int twos=0;
+        for(int num:nums){
+            ones=(ones^num)&(~twos);
+            twos=(twos^num)&(~ones);
+        }
+        return ones;
+    }
+}
