6c9c984df0afda66521115b3d55f52d91681e56f
Akshit Kumar Chandora <91782956+axitchandora@users.noreply.github.com>
2022-07-24 02:28:46 +0530

    Time: 2 ms (54.92%), Space: 44.9 MB (66.34%) - LeetHub

--- 260-single-number-iii/260-single-number-iii.java
+++ 260-single-number-iii/260-single-number-iii.java
@@ -2,19 +2,19 @@ class Solution {
     public int[] singleNumber(int[] nums) {
         // get the XOR of the all the numbers
         int n1xn2=0;
-        for(int num:nums)
-            n1xn2 ^=num;
+        for(int n=0;n<nums.length;n++)
+            n1xn2 ^=nums[n];
         int rightmostSetBit=1;
         // get the rightmost bit that is '1'
         while((rightmostSetBit & n1xn2)==0 ){
             rightmostSetBit=rightmostSetBit<<1;
         }
         int num1=0,num2=0;
-        for(int num:nums){
-            if((num & rightmostSetBit)!=0) // the bit is set
-                num1 ^=num;
+        for(int n=0;n<nums.length;n++){
+            if((nums[n] & rightmostSetBit)!=0) // the bit is set
+                num1 ^=nums[n];
             else // the bit is not set
-                num2 ^=num;
+                num2 ^=nums[n];
         }
         return new int[]{num1,num2};
     }
