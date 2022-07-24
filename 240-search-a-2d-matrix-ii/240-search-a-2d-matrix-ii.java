1a5cfe10ce464283523c53c6c684cfe178d3a637
Akshit Kumar Chandora <91782956+axitchandora@users.noreply.github.com>
2022-07-24 16:11:15 +0530

    Time: 15 ms (21.26%), Space: 57.5 MB (60.49%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.java
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.java
@@ -3,7 +3,7 @@ class Solution {
         int row=0,column=matrix[0].length-1;
         while(row<matrix.length && column>=0){
             if(matrix[row][column]==target) return true;
-            else if(matrix[row][column]<target) row++;
+            if(matrix[row][column]<target) row++;
             else column--;
         }
         return false;
