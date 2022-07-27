c757661011e44f7b353465832c8e50e7daca86c9
Akshit Kumar Chandora <91782956+axitchandora@users.noreply.github.com>
2022-07-27 17:10:57 +0530

    Time: 2 ms (76.41%), Space: 44.8 MB (45.70%) - LeetHub

--- 46-permutations/46-permutations.java
+++ 46-permutations/46-permutations.java
@@ -1,18 +1,17 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> result=new ArrayList();
-        generatePermutationsRecursive(nums,0,new ArrayList(),result);
+        List<List<Integer>> result=new ArrayList<>();
+        generatePermutationsRecursive(nums,0,new ArrayList<Integer>(),result);
         return result;
     }
     private static void generatePermutationsRecursive(int[] nums, int index, List<Integer> currentPermutation,
       List<List<Integer>> result){
-        if(currentPermutation.size()==nums.length){
+        if(index==nums.length){
             result.add(currentPermutation);
-            return;
         }else{
-            
+            // create a new permutation by adding the current number at every position
             for(int i=0;i<=currentPermutation.size();i++){
-                List<Integer> newPermutation=new ArrayList(currentPermutation);
+                List<Integer> newPermutation=new ArrayList<Integer>(currentPermutation);
                 newPermutation.add(i,nums[index]);
                 generatePermutationsRecursive(nums,index+1,newPermutation,result);
             }
