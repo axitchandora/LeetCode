81cf7ba9ea46d44f8a97c42e75315bda9731b6fe
Akshit Kumar Chandora <91782956+axitchandora@users.noreply.github.com>
2022-07-27 14:42:33 +0530

    Time: 3 ms (48.03%), Space: 44.3 MB (32.21%) - LeetHub

---
+++ 90-subsets-ii/90-subsets-ii.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        // sort the numbers to handle duplicates
+        Arrays.sort(nums);
+        List<List<Integer>> subsets=new ArrayList();
+        subsets.add(new ArrayList());
+        int startIndex=0, endIndex=0;
+        for(int i=0;i<nums.length;i++){
+            startIndex=0;
+            // if current and the previous elements are same, create new subsets only from the subsets 
+            // added in the previous step
+            if(i>0 && nums[i]==nums[i-1])
+                startIndex=endIndex+1;
+            endIndex=subsets.size()-1;
+            for(int j=startIndex;j<=endIndex;j++){
+                // create a new subset from the existing subset and add the current element to it
+                List<Integer> set=new ArrayList<>(subsets.get(j));
+                set.add(nums[i]);
+                subsets.add(set);
+            }
+        }
+        return subsets;
+    }
+}
