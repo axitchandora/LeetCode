6c722bc3772e4078cee304404d26a1203220597d
Akshit Kumar Chandora <91782956+axitchandora@users.noreply.github.com>
2022-07-28 01:03:42 +0530

    Time: 18 ms (28.91%), Space: 44.4 MB (67.71%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.java
+++ 9-palindrome-number/9-palindrome-number.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if(x<0)
+        if(x<0 || (x % 10 == 0 && x != 0))
             return false;
         int rev=0;
         int num=x;
