50ada42dd0a592f9e3f37869ace65e0c57ea2d0f
Akshit Kumar Chandora <91782956+axitchandora@users.noreply.github.com>
2022-08-01 22:56:42 +0530

    Time: 1 ms (97.56%), Space: 43 MB (82.87%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.java
+++ 22-generate-parentheses/22-generate-parentheses.java
@@ -1,32 +1,26 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
-        List<String> result=new ArrayList();
-        Queue<ParenthesesString> queue=new LinkedList();
-        queue.add(new ParenthesesString("",0,0));
-        while(!queue.isEmpty()){
-            ParenthesesString ps=queue.poll();
-            // if we've reached the maximum number of open and close parentheses, add to the result
-            if(ps.openCount==n && ps.closeCount==n){
-                result.add(ps.str);
-            }else{
-                if(ps.openCount<n){ // if we can add an open parentheses, add it
-                    queue.add(new ParenthesesString(ps.str+"(",ps.openCount+1,ps.closeCount));
-                }
-                if(ps.openCount>ps.closeCount){ // if we can add a close parentheses, add it
-                    queue.add(new ParenthesesString(ps.str+")",ps.openCount,ps.closeCount+1));
-                }
-            }
-        }
+        List<String> result=new ArrayList<>();
+        char [] parenthesesString=new char[2*n];
+        generateValidParenthesesRecursive(n,0,0,parenthesesString,0,result);
         return result;
     }
-    static class ParenthesesString{
-        String str;
-        int openCount; // open parentheses count
-        int closeCount; // close parentheses count
-        public ParenthesesString(String str,int openCount,int closeCount){
-            this.str=str;
-            this.openCount=openCount;
-            this.closeCount=closeCount;
+    private static void generateValidParenthesesRecursive(int num, int openCount, 
+      int closeCount, char[] parenthesesString, int index, List<String> result){
+        // if we've reached the maximum number of open and close parentheses, add to the result
+        if(openCount==num && closeCount==num){
+            result.add(new String(parenthesesString));
+        }else{
+            if(openCount<num){ // if we can add an open parentheses, add it
+                parenthesesString[index]='(';
+                generateValidParenthesesRecursive(num,openCount+1,closeCount,
+                                                  parenthesesString,index+1,result);
+            }
+            if(openCount>closeCount){ // if we can add a close parentheses, add it
+                parenthesesString[index]=')';
+                generateValidParenthesesRecursive(num,openCount,closeCount+1,
+                                                  parenthesesString,index+1,result);
+            }
         }
     }
 }
