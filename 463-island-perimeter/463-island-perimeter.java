3b17104ca2349eb029afb916b074332d4dc10486
Akshit Kumar Chandora <91782956+axitchandora@users.noreply.github.com>
2022-08-03 00:23:57 +0530

    Time: 14 ms (31.89%), Space: 63.3 MB (15.72%) - LeetHub

--- 463-island-perimeter/463-island-perimeter.java
+++ 463-island-perimeter/463-island-perimeter.java
@@ -12,19 +12,24 @@ class Solution {
         }
         return 0;
     }
-    private int[][] dirs={{0,1},{1,0},{0,-1},{-1,0}};
-    private int islandPerimeterDFS(int[][] matrix,boolean[][] visited, int x,int y){
-        if(x<0 || x>= matrix.length || y<0 || y>= matrix[0].length)
-            return 1;
-        if(matrix[x][y]==0)
-            return 1;
-        if(visited[x][y])
-            return 0;
-        visited[x][y]=true;
-        int edgeCount=0;
-        for(int[] z:dirs){
-            edgeCount+=islandPerimeterDFS(matrix, visited, x+z[0], y+z[1]);
-        }
+  private static int islandPerimeterDFS(int[][] matrix, boolean[][] visited, int x, int y) {
+        if (x < 0 || x >= matrix.length || y < 0 || y >= matrix[0].length)
+          return 1; // returning 1, since this a boundary cell initiated this DFS call
+        if (matrix[x][y] == 0)
+          return 1; // returning 1, because of the shared side b/w a water and a land cell
+
+        if (visited[x][y])
+          return 0; // we have already taken care of this cell
+
+        visited[x][y] = true; // mark the cell visited
+
+        int edgeCount = 0;
+        // recursively visit all neighboring cells (horizontally & vertically)
+        edgeCount += islandPerimeterDFS(matrix, visited, x + 1, y); // lower cell
+        edgeCount += islandPerimeterDFS(matrix, visited, x - 1, y); // upper cell
+        edgeCount += islandPerimeterDFS(matrix, visited, x, y + 1); // right cell
+        edgeCount += islandPerimeterDFS(matrix, visited, x, y - 1); // left cell
+
         return edgeCount;
-    }
+  }
 }
