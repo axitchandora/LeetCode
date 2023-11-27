d0df688a9d80146b8d71fba30bbe8aec71d770d2
Akshit Chandora <91782956+axitchandora@users.noreply.github.com>
2023-11-27 21:58:50 +0530

    Time: 6 ms (98.37%), Space: 40 MB (93.75%) - LeetHub

---
+++ 0935-knight-dialer/0935-knight-dialer.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int knightDialer(int n) {
+        if (n == 1) {
+            return 10;
+        }
+        
+        int A = 4;
+        int B = 2;
+        int C = 2;
+        int D = 1;
+        int MOD = (int) 1e9 + 7;
+        
+        for (int i = 0; i < n - 1; i++) {
+            int tempA = A;
+            int tempB = B;
+            int tempC = C;
+            int tempD = D;
+            
+            A = ((2 * tempB) % MOD + (2 * tempC) % MOD) % MOD;
+            B = tempA;
+            C = (tempA + (2 * tempD) % MOD) % MOD;
+            D = tempC;
+        }
+        
+        int ans = (A + B) % MOD;
+        ans = (ans + C) % MOD;
+        return (ans + D) % MOD;
+    }
+}
