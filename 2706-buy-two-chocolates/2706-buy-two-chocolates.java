7f1de9636e7604ef53f01581502e260b3537386e
Akshit Chandora <91782956+axitchandora@users.noreply.github.com>
2023-12-20 20:44:13 +0530

    Time: 1 ms (100.00%), Space: 44.9 MB (5.53%) - LeetHub

---
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int buyChoco(int[] prices, int money) {
+        // Assume minimum and second minimum
+        int minimum = Math.min(prices[0], prices[1]);
+        int secondMinimum = Math.max(prices[0], prices[1]);
+
+        // Iterate over the remaining elements
+        for (int i = 2; i < prices.length; i++) {
+            if (prices[i] < minimum) {
+                secondMinimum = minimum;
+                minimum = prices[i];
+            } else if (prices[i] < secondMinimum) {
+                secondMinimum = prices[i];
+            }
+        }
+
+        // Minimum Cost
+        int minCost = minimum + secondMinimum;
+
+        // We can buy chocolates only if we have enough money
+        if (minCost <= money) {
+            // Return the Amount of Money Left
+            return money - minCost;
+        }
+
+        // We cannot buy chocolates. Return the initial amount of money
+        return money;
+    }
+}
