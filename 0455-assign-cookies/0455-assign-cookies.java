f0302d058873c336b808d37de6f13a54b3805ebb
Akshit Chandora <91782956+axitchandora@users.noreply.github.com>
2024-01-01 13:56:27 +0530

    Time: 10 ms (13.48%), Space: 45.1 MB (17.69%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int contentChildren = 0;
+        int cookieIndex = 0;
+        while (cookieIndex < s.length && contentChildren < g.length) {
+            if (s[cookieIndex] >= g[contentChildren]) {
+                contentChildren++;
+            }
+            cookieIndex++;
+        }
+        return contentChildren;
+    }
+
+}
