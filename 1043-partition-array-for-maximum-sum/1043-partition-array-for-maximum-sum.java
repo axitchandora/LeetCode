401768ffa9b3b1223ff12f006bf8cc0767cec3ad
Akshit Chandora <91782956+axitchandora@users.noreply.github.com>
2024-02-03 20:23:51 +0530

    Time: 6 ms (93.17%), Space: 42.3 MB (30.35%) - LeetHub

---
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxSumAfterPartitioning(int[] arr, int k) {
+        int N = arr.length;
+
+        int[] dp = new int[N + 1];
+        Arrays.fill(dp, 0);
+
+        for (int start = N - 1; start >= 0; start--) {
+            int currMax = 0;
+            int end = Math.min(N, start + k);
+
+            for (int i = start; i < end; i++) {
+                currMax = Math.max(currMax, arr[i]);
+                // Store the maximum of all options for the current subarray.
+                dp[start] = Math.max(dp[start], dp[i + 1] + currMax * (i - start + 1));
+            }
+        }
+        return dp[0];
+    }
+}
