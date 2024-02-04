0923d59aea154cc35145c0d6f9c20a68c776f9b6
Akshit Chandora <91782956+axitchandora@users.noreply.github.com>
2024-02-04 18:05:28 +0530

    Time: 17 ms (45.15%), Space: 45.2 MB (33.54%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -0,0 +1,51 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        if (s.isEmpty() || t.isEmpty()) {
+            return "";
+        }
+
+        Map<Character, Integer> dictT = new HashMap<>();
+        for (char c : t.toCharArray()) {
+            int count = dictT.getOrDefault(c, 0);
+            dictT.put(c, count + 1);
+        }
+
+        int required = dictT.size();
+        int l = 0, r = 0;
+        int formed = 0;
+
+        Map<Character, Integer> windowCounts = new HashMap<>();
+        int[] ans = { -1, 0, 0 };
+
+        while (r < s.length()) {
+            char c = s.charAt(r);
+            int count = windowCounts.getOrDefault(c, 0);
+            windowCounts.put(c, count + 1);
+
+            if (dictT.containsKey(c) && windowCounts.get(c).intValue() == dictT.get(c).intValue()) {
+                formed++;
+            }
+
+            while (l <= r && formed == required) {
+                c = s.charAt(l);
+
+                if (ans[0] == -1 || r - l + 1 < ans[0]) {
+                    ans[0] = r - l + 1;
+                    ans[1] = l;
+                    ans[2] = r;
+                }
+
+                windowCounts.put(c, windowCounts.get(c) - 1);
+                if (dictT.containsKey(c) && windowCounts.get(c).intValue() < dictT.get(c).intValue()) {
+                    formed--;
+                }
+
+                l++;
+            }
+
+            r++;
+        }
+
+        return ans[0] == -1 ? "" : s.substring(ans[1], ans[2] + 1);
+    }
+}
