d342f3d832f86e90ac1fc18987a5e99dbf674edc
Akshit Chandora <91782956+axitchandora@users.noreply.github.com>
2024-02-07 22:10:02 +0530

    Time: 13 ms (77.46%), Space: 45.9 MB (24.30%) - LeetHub

---
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String frequencySort(String s) {
+
+        // Count up the occurances.
+        Map<Character, Integer> counts = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            counts.put(c, counts.getOrDefault(c, 0) + 1);
+        }
+
+        // Make a list of the keys, sorted by frequency.
+        List<Character> characters = new ArrayList<>(counts.keySet());        
+        Collections.sort(characters, (a, b) -> counts.get(b) - counts.get(a));
+
+        // Convert the counts into a string with a sb.
+        StringBuilder sb = new StringBuilder();
+        for (char c : characters) {
+            int copies = counts.get(c);
+            for (int i = 0; i < copies; i++) {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+}
