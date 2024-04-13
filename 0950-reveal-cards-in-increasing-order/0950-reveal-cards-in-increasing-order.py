abf8c7df3afa91877643585e0ccfec158a781dac
Akshit Chandora <91782956+axitchandora@users.noreply.github.com>
2024-04-13 17:29:12 +0530

    Time: 50 ms (28.01%), Space: 16.8 MB (76.00%) - LeetHub

--- 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.py
+++ 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.py
@@ -1,19 +1,15 @@
 class Solution:
     def deckRevealedIncreasing(self, deck: List[int]) -> List[int]:
-        N = len(deck)
-        result = [0] * N
-        skip = False
-        index_in_deck = 0
-        index_in_result = 0
-
         deck.sort()
-
-        while index_in_deck < N:
-            if result[index_in_result] == 0:
-                if not skip:
-                    result[index_in_result] = deck[index_in_deck]
-                    index_in_deck += 1
-                skip = not skip
-            index_in_result = (index_in_result + 1) % N
-
-        return result
+        res= [0]*len(deck)
+        q = deque(range(len(deck)))
+        
+        for n in deck:
+            i= q.popleft()
+            res[i] = n
+            if q:
+                q.append(q.popleft())
+        return res
+            
+        
+        
