c3b9ab0da579c8c0e333a21b403c9e1797af125f
Akshit Chandora <91782956+axitchandora@users.noreply.github.com>
2024-05-14 23:20:52 +0530

    Time: 148 ms (86.26%), Space: 19.1 MB (7.44%) - LeetHub

--- 0857-minimum-cost-to-hire-k-workers/0857-minimum-cost-to-hire-k-workers.py
+++ 0857-minimum-cost-to-hire-k-workers/0857-minimum-cost-to-hire-k-workers.py
@@ -1,24 +1,21 @@
 class Solution:
     def mincostToHireWorkers(self, quality: List[int], wage: List[int], k: int) -> float:
-        n = len(quality)
-        total_cost = float("inf")
-        current_total_quality = 0
-        wage_to_quality_ratio = []
-        for i in range(n):
-            wage_to_quality_ratio.append((wage[i] / quality[i], quality[i]))
-        wage_to_quality_ratio.sort(key=lambda x: x[0])
-        highest_quality_workers = []
-
-        for i in range(n):
-            heapq.heappush(highest_quality_workers, -wage_to_quality_ratio[i][1])
-            current_total_quality += wage_to_quality_ratio[i][1]
-
-            if len(highest_quality_workers) > k:
-                current_total_quality += heapq.heappop(highest_quality_workers)
-
-            if len(highest_quality_workers) == k:
-                total_cost = min(
-                    total_cost, current_total_quality * wage_to_quality_ratio[i][0]
-                )
-
-        return total_cost
+        res = float("inf")
+        pairs = [] # (ratio, quality)
+        for i in range(len(quality)):
+            pairs.append(((wage[i]/quality[i]),quality[i]))
+        pairs.sort(key=lambda p:p[0])
+        total_quality = 0
+        maxheap = [] #quailty
+        for ratio,q in pairs:
+            heapq.heappush(maxheap, -q)
+            total_quality += q
+            
+            if(len(maxheap)>k):
+                total_quality += heapq.heappop(maxheap)
+            if(len(maxheap)==k):
+                res = min(res, total_quality*ratio)
+            
+        return res
+        
+        
