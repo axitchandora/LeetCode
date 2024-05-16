cb4f21a02222125abe945b62376f9680b75ac15e
Akshit Chandora <91782956+axitchandora@users.noreply.github.com>
2024-05-16 23:35:07 +0530

    Time: 52 ms (23.68%), Space: 16.8 MB (92.02%) - LeetHub

--- 2331-evaluate-boolean-binary-tree/2331-evaluate-boolean-binary-tree.py
+++ 2331-evaluate-boolean-binary-tree/2331-evaluate-boolean-binary-tree.py
@@ -6,11 +6,24 @@
 #         self.right = right
 class Solution:
     def evaluateTree(self, root: Optional[TreeNode]) -> bool:
-        if not root.left:
-            return root.val == 1
-        if(root.val == 2):
-            return (self.evaluateTree(root.left) or self.evaluateTree(root.right))
-        if(root.val == 3):
-            return (self.evaluateTree(root.left) and self.evaluateTree(root.right))
+        stack = [root]
+        value = {} # map node -> value
+        while stack:
+            node = stack.pop()
+            # Leaf Node
+            if not node.left:
+                value[node] = node.val == 1
+            #Non-leaf Node
+            else:
+                # Children visited
+                if node.left in value:
+                    if node.val == 2:
+                        value[node] = value[node.left] or value[node.right]
+                    if node.val == 3:
+                        value[node] = value[node.left] and value[node.right]
+                # Otherwise add children
+                else:
+                    stack.extend([node, node.left, node.right])
+        return value[root]
         
         
