42566fcea448a8008380452f38c60525904374a2
Akshit Chandora <91782956+axitchandora@users.noreply.github.com>
2024-07-08 23:10:50 +0530

    Time: 31 ms (90.69%), Space: 16.5 MB (49.47%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
@@ -1,14 +1,10 @@
 class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
-        q = deque()
-        
-        for i in range(1 , n + 1):
-            q.append(i)
+
+        def helper(n, k):
+            if n == 1:
+                return 0
             
-        while len(q) > 1:
-            for i in range(k - 1):
-                q.append(q.popleft())
-            q.popleft()
-            
-        return q[0]
+            return (helper(n - 1, k) + k) % n
+        return helper(n, k) + 1
         
