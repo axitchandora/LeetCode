2f9a32be0fa3432dcdb01f9546ce437a0ce202a8
Akshit Chandora <91782956+axitchandora@users.noreply.github.com>
2024-07-18 23:40:35 +0530

    Time: 76 ms (86.77%), Space: 16.9 MB (38.59%) - LeetHub

--- 1530-number-of-good-leaf-nodes-pairs/1530-number-of-good-leaf-nodes-pairs.py
+++ 1530-number-of-good-leaf-nodes-pairs/1530-number-of-good-leaf-nodes-pairs.py
@@ -10,9 +10,11 @@ class Solution:
         self.pairs = 0
         def dfs(node):
             if not node:
-                return []
+                return defaultdict(int)
             if not node.left and not node.right:
-                return [1]
+                count = defaultdict(int)
+                count[1] = 1
+                return count
             
             left_dist = dfs(node.left)
             right_dist = dfs(node.right)
@@ -20,10 +22,18 @@ class Solution:
             for d1 in left_dist:
                 for d2 in right_dist:
                     if d1 + d2 <= distance:
-                        self.pairs += 1
+                        self.pairs += left_dist[d1] * right_dist[d2]
             
-            all_dist = left_dist + right_dist
-            return [d + 1 for d in all_dist]
+            all_dist = defaultdict(int)
+            for d in left_dist:
+                if d + 1 <= distance:
+                    all_dist[d + 1] = left_dist[d]
+                    
+            for d in right_dist:
+                if d + 1 <= distance:
+                    all_dist[d + 1] += right_dist[d]
+            
+            return all_dist
         
         dfs(root)
         return self.pairs
