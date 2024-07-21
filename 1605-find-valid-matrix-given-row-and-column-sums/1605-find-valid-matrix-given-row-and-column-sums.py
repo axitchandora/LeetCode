66bf3cbe321e1a213d3f6ece6ec75ef7e97ef1d6
Akshit Chandora <91782956+axitchandora@users.noreply.github.com>
2024-07-21 22:50:25 +0530

    Time: 510 ms (71.18%), Space: 21.8 MB (35.76%) - LeetHub

--- 1605-find-valid-matrix-given-row-and-column-sums/1605-find-valid-matrix-given-row-and-column-sums.py
+++ 1605-find-valid-matrix-given-row-and-column-sums/1605-find-valid-matrix-given-row-and-column-sums.py
@@ -1,19 +1,26 @@
 class Solution:
-    def restoreMatrix(self, rowSum, colSum):
-        N = len(rowSum)
-        M = len(colSum)
-
-        curr_row_sum = [0] * N
-        curr_col_sum = [0] * M
-
-        orig_matrix = [[0] * M for _ in range(N)]
-        for i in range(N):
-            for j in range(M):
-                orig_matrix[i][j] = min(
-                    rowSum[i] - curr_row_sum[i], colSum[j] - curr_col_sum[j]
-                )
-
-                curr_row_sum[i] += orig_matrix[i][j]
-                curr_col_sum[j] += orig_matrix[i][j]
-
-        return orig_matrix
+    def restoreMatrix(self, rowSum: List[int], colSum: List[int]) -> List[List[int]]:
+        ROWS, COLS = len(rowSum) , len(colSum)
+        
+        res = [[0] * COLS for _ in range(ROWS)]
+        
+        for r in range(ROWS):
+            res[r][0] = rowSum[r]
+            
+        for c in range(COLS):
+            cur_col_sum = 0
+            #cur_col_sum = sum([res[r][c] for r in range(ROWS)])
+            for r in range(ROWS):
+                cur_col_sum += res[r][c]
+                
+            r = 0
+            while cur_col_sum > colSum[c]:
+                diff = cur_col_sum - colSum[c]
+                max_shift = min(res[r][c], diff)
+            
+                res[r][c] -= max_shift
+                res[r][c+1] += max_shift
+                cur_col_sum -= max_shift
+                r += 1
+                
+        return res
