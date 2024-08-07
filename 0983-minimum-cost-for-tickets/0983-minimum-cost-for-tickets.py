2f62cef42406b154bd8fb19758470a1b6ba81360
Akshit Chandora <91782956+axitchandora@users.noreply.github.com>
2024-08-08 00:06:06 +0530

    Time: 39 ms (85.72%), Space: 16.5 MB (70.41%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.py
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.py
@@ -3,19 +3,13 @@ class Solution:
         # Top to Bottom DP
         dp = {}
         
-        def dfs(i):
-            if i == len(days):
-                return 0
-            if i in dp:
-                return dp[i]
-            
+        for i in range(len(days)-1 , -1 , -1):           
             dp[i] = float("inf")
             
             for d,c in zip([1,7,30],costs):
                 j = i
                 while j < len(days) and days[j] < days[i] + d:
                     j += 1
-                dp[i] = min(dp[i], c + dfs(j))
+                dp[i] = min(dp[i], c + dp.get(j,0))
             
-            return dp[i]
-        return dfs(0)
+        return dp[0]
