b2ab0a92b2dbd0f4d67504503dca4cc28526836f
Akshit Chandora <91782956+axitchandora@users.noreply.github.com>
2024-08-24 18:02:27 +0530

    Time: 45 ms (6.61%), Space: 16.5 MB (92.31%) - LeetHub

---
+++ 0564-find-the-closest-palindrome/0564-find-the-closest-palindrome.py
@@ -0,0 +1,39 @@
+class Solution:
+    def nearestPalindromic(self, n: str) -> str:
+        len_n = len(n)
+        i = len_n // 2 - 1 if len_n % 2 == 0 else len_n // 2
+        first_half = int(n[: i + 1])
+        possibilities = []
+        possibilities.append(
+            self.half_to_palindrome(first_half, len_n % 2 == 0)
+        )
+        possibilities.append(
+            self.half_to_palindrome(first_half + 1, len_n % 2 == 0)
+        )
+        possibilities.append(
+            self.half_to_palindrome(first_half - 1, len_n % 2 == 0)
+        )
+        possibilities.append(10 ** (len_n - 1) - 1)
+        possibilities.append(10**len_n + 1)
+
+        diff = float("inf")
+        res = 0
+        nl = int(n)
+        for cand in possibilities:
+            if cand == nl:
+                continue
+            if abs(cand - nl) < diff:
+                diff = abs(cand - nl)
+                res = cand
+            elif abs(cand - nl) == diff:
+                res = min(res, cand)
+        return str(res)
+
+    def half_to_palindrome(self, left: int, even: bool) -> int:
+        res = left
+        if not even:
+            left = left // 10
+        while left > 0:
+            res = res * 10 + left % 10
+            left //= 10
+        return res
