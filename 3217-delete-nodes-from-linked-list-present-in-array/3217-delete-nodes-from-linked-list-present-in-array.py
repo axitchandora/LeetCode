717d5ae75242e84313f619a21931a65069cdf09d
Akshit Chandora <91782956+axitchandora@users.noreply.github.com>
2024-09-06 22:21:48 +0530

    Time: 722 ms (55.34%), Space: 55 MB (63.82%) - LeetHub

--- 3217-delete-nodes-from-linked-list-present-in-array/3217-delete-nodes-from-linked-list-present-in-array.py
+++ 3217-delete-nodes-from-linked-list-present-in-array/3217-delete-nodes-from-linked-list-present-in-array.py
@@ -9,7 +9,7 @@ class Solution:
         dummy_node = ListNode(-1)
         dummy_node.next = head
         tmp_head = dummy_node
-        while tmp_head and tmp_head.next:
+        while tmp_head:
             while tmp_head.next and tmp_head.next.val in num_set:
                 tmp_head.next = tmp_head.next.next            
             tmp_head = tmp_head.next
