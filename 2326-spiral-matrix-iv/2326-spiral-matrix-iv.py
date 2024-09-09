382b7bd02e7e0b0ca2eee262852d629bb95737ff
Akshit Chandora <91782956+axitchandora@users.noreply.github.com>
2024-09-09 23:04:35 +0530

    Time: 629 ms (86.67%), Space: 54.9 MB (60.00%) - LeetHub

--- 2326-spiral-matrix-iv/2326-spiral-matrix-iv.py
+++ 2326-spiral-matrix-iv/2326-spiral-matrix-iv.py
@@ -6,45 +6,43 @@
 class Solution:
     def spiralMatrix(self, m: int, n: int, head: Optional[ListNode]) -> List[List[int]]:
         left, right = 0 , n
-        top, bottom = 0 , m
-        res = [[-1 for i in range(n)] for j in range(m)]
-        tmp = head
-        while left < right and top < bottom and tmp:
-            
+        top, bottom = 0, m
+        grid = [[-1] * n for _ in range(m)]
+        
+        while head:
+            # left to right
             for i in range(left, right):
-                if tmp:
-                    res[top][i] = tmp.val
-                else:
-                    return res
-                tmp = tmp.next
+                if not head:
+                    return grid
+                grid[top][i] = head.val
+                head = head.next
             top += 1
             
+            # top to bottom
             for i in range(top, bottom):
-                if tmp:
-                    res[i][right-1] = tmp.val
-                else:
-                    return res
-                tmp = tmp.next
+                if not head:
+                    return grid
+                grid[i][right-1] = head.val
+                head = head.next
             right -= 1
             
-            if not (left < right and top < bottom):
-                break
-                
+            # right to left
+            
             for i in range(right - 1 , left - 1, -1):
-                if tmp:
-                    res[bottom-1][i] = tmp.val
-                else:
-                    return res
-                tmp = tmp.next
+                if not head:
+                    return grid
+                grid[bottom - 1][i] = head.val
+                head = head.next
             bottom -= 1
             
-            for i in range(bottom -1, top - 1, -1):
-                if tmp:
-                    res[i][left] = tmp.val
-                else:
-                    return res
-                tmp = tmp.next
-            left += 1
+            # bottom to top
             
-        return res
+            for i in range(bottom - 1 , top - 1, -1):
+                if not head:
+                    return grid
+                grid[i][left] = head.val
+                head = head.next
+            left += 1
+        
+        return grid
         
