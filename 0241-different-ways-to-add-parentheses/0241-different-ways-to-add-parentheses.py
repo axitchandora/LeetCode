9b7fb0de5ba75e24f863e3db4e549f12808ec078
Akshit Chandora <91782956+axitchandora@users.noreply.github.com>
2024-09-19 22:10:11 +0530

    Time: 33 ms (83.91%), Space: 16.7 MB (67.70%) - LeetHub

--- 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.py
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.py
@@ -1,25 +1,23 @@
 class Solution:
-    def diffWaysToCompute(self, expression: str) -> List[int]:
-        operations = {
+    operations = {
             "+": lambda x, y: x + y,
             "-": lambda x, y: x - y,
             "*": lambda x, y: x * y,
         }
+    def diffWaysToCompute(self, expression: str) -> List[int]:
         
-        def backtrack(left, right):
-            res = []
-            
-            for i in range(left, right + 1):
-                op = expression[i]
-                if op in operations:
-                    nums1 = backtrack(left, i - 1)
-                    nums2 = backtrack(i + 1, right)
-                    
-                    for n1 in nums1:
-                        for n2 in nums2:
-                            res.append(operations[op](n1,n2))
-            if res == []:
-                res.append(int(expression[left:right+1]))
-            return res
+        res = []
+
+        for i in range(len(expression)):
+            op = expression[i]
+            if op in self.operations:
+                nums1 = self.diffWaysToCompute(expression[:i])
+                nums2 = self.diffWaysToCompute(expression[i+1:])
+
+                for n1 in nums1:
+                    for n2 in nums2:
+                        res.append(self.operations[op](n1,n2))
+        if res == []:
+            res.append(int(expression))
+        return res
         
-        return backtrack(0, len(expression) - 1)
