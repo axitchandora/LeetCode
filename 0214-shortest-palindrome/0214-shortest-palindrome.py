2c0760b753cb0e216906e7667989ff2fd25df4f5
Akshit Chandora <91782956+axitchandora@users.noreply.github.com>
2024-09-20 22:31:32 +0530

    Time: 64 ms (66.52%), Space: 17.1 MB (41.65%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.py
+++ 0214-shortest-palindrome/0214-shortest-palindrome.py
@@ -1,16 +1,24 @@
 class Solution:
     def shortestPalindrome(self, s: str) -> str:
-        def is_pali(s, l, r):
-            while l <= r:
-                if s[l] != s[r]:
-                    return False
-                l, r = l + 1 , r - 1
-            return True
+        prefix = 0
+        suffix = 0
+        base = 29
+        last_index = 0 # -1
+        power = 1
+        mod = 10**9 + 7
         
-        for r in reversed(range(len(s))):
-            if is_pali(s, 0 , r):
-                suffix = s[r+1:]
-                return suffix[::-1]+s
+        for i , c in enumerate(s):
+            char = (ord(c) - ord('a') + 1)
             
-        return ""
+            prefix = (prefix * base) % mod
+            prefix = (prefix + char) % mod
+            
+            suffix = (suffix + char * power) % mod
+            power = (power * base) % mod
+            
+            if prefix == suffix:
+                last_index = i
+            
+        suffix = s[last_index + 1 :]
+        return suffix[::-1] + s
         
