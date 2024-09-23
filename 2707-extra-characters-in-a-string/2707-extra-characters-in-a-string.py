8ee4e5133afa04d2f947e132c1c621e06b53b353
Akshit Chandora <91782956+axitchandora@users.noreply.github.com>
2024-09-24 00:05:44 +0530

    Time: 183 ms (82.31%), Space: 20.2 MB (10.68%) - LeetHub

--- 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.py
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.py
@@ -1,16 +1,35 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.word = False
+        
+class Trie:
+    def __init__(self, words):
+        self.root = TrieNode()
+        for w in words:
+            curr = self.root
+            for c in w:
+                if c not in curr.children:
+                    curr.children[c] = TrieNode()
+                curr = curr.children[c]
+            curr.word = True
 class Solution:
     def minExtraChar(self, s: str, dictionary: List[str]) -> int:
-        words = set(dictionary)
         dp = { len(s): 0}
+        trie = Trie(dictionary).root
         
         def dfs(i):
             if i in dp:
                 return dp[i]
             
             res = 1 + dfs(i + 1 ) # skip curr char
+            curr = trie
             for j in range(i, len(s)):
-                if s[i:j+1] in words:
-                    res = min(res, dfs(j + 1))
+                if s[j] not in curr.children:
+                    break
+                curr = curr.children[s[j]]
+                if curr.word:
+                    res = min(res,dfs(j + 1))
             dp[i] = res
             return res
         
