f25e1653533094e2353e2332619615cecdd124bf
Akshit Chandora <91782956+axitchandora@users.noreply.github.com>
2024-09-25 22:29:30 +0530

    Time: 3042 ms (30.00%), Space: 384.4 MB (32.07%) - LeetHub

--- 2416-sum-of-prefix-scores-of-strings/2416-sum-of-prefix-scores-of-strings.py
+++ 2416-sum-of-prefix-scores-of-strings/2416-sum-of-prefix-scores-of-strings.py
@@ -1,18 +1,40 @@
+class PrefixNode:
+    def __init__(self):
+        self.children = [None] * 26
+        self.count = 0
+
+class PrefixTree:
+    def __init__(self):
+        self.root = PrefixNode()
+        
+    def insert(self, w):
+        cur = self.root
+        for c in w:
+            i = ord(c) - ord('a')
+            if not cur.children[i]:
+                cur.children[i] = PrefixNode()
+            cur = cur.children[i]
+            cur.count += 1
+
+    def get_score(self, w):
+        cur = self.root
+        score = 0
+        for c in w:
+            i = ord(c) - ord('a')
+            cur = cur.children[i]
+            score += cur.count
+        return score
+
 class Solution:
     def sumPrefixScores(self, words: List[str]) -> List[int]:
-        res = []
-        
-        count = defaultdict(int)
+        res = []        
+        prefix_tree = PrefixTree()
         
         for w in words:
-            for i in range(len(w)):
-                count[w[:i+1]] += 1
-                
-        for w in words:
-            score = 0
-            for i in range(len(w)):
-                score += count[w[:i+1]]
-            res.append(score)
+            prefix_tree.insert(w)
             
+        for w in words:
+            res.append(prefix_tree.get_score(w))
+        
         return res
         
