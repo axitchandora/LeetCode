f451e1776ed44ac91fc7d17ecb900a1e660bf90e
Akshit Chandora <91782956+axitchandora@users.noreply.github.com>
2024-09-26 22:44:37 +0530

    Time: 141 ms (99.63%), Space: 17.4 MB (65.25%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.py
+++ 0729-my-calendar-i/0729-my-calendar-i.py
@@ -1,16 +1,37 @@
+class Tree:
+    def __init__(self, start, end):
+        self.left = None
+        self.right = None
+        self.start = start
+        self.end = end
+        
+    def insert(self, start, end):
+        curr =self
+        while True:
+            if start >= curr.end:
+                if not curr.right:
+                    curr.right = Tree(start, end)
+                    return True
+                curr = curr.right
+            elif end <= curr.start:
+                if not curr.left:
+                    curr.left = Tree(start, end)
+                    return True
+                curr = curr.left
+            else:
+                return False
+        return True
+
 class MyCalendar:
 
     def __init__(self):
-        self.events = [] # (s, e)
+        self.root = None
 
     def book(self, start: int, end: int) -> bool:
-        for s, e in self.events:
-            if e > start and end > s:
-                return False
-        self.events.append((start, end))
-        return True
-            
-
+        if not self.root:
+            self.root = Tree(start,end)
+            return True
+        return self.root.insert(start, end)
 
 # Your MyCalendar object will be instantiated and called as such:
 # obj = MyCalendar()
