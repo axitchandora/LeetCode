6896e04c8bde13b19456fa10b8b1d27b2368045a
Akshit Chandora <91782956+axitchandora@users.noreply.github.com>
2024-10-04 22:30:44 +0530

    Time: 1371 ms (22.26%), Space: 53.6 MB (73.37%) - LeetHub

---
+++ 3295-report-spam-message/3295-report-spam-message.py
@@ -0,0 +1,10 @@
+class Solution:
+    def reportSpam(self, message: List[str], bannedWords: List[str]) -> bool:
+        count = Counter(bannedWords)
+        words = 0
+        for mess in message:
+            if mess in count:
+                words += 1
+            if words == 2:
+                return True
+        return False
