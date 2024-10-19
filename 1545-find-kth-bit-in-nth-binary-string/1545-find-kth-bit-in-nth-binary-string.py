2972f7534316c5614d5cf78d55fc449413c1d012
Akshit Chandora <91782956+axitchandora@users.noreply.github.com>
2024-10-19 17:35:19 +0530

    Time: 0 ms (100.00%), Space: 16.5 MB (88.95%) - LeetHub

--- 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.py
+++ 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.py
@@ -1,19 +1,29 @@
 class Solution:
     def findKthBit(self, n: int, k: int) -> str:
         length = 2**n - 1
+#         invert = False
+#         while length > 1:
+#             half = length // 2
+            
+#             if k <= half:
+#                 length = half
+#             elif k > half + 1:
+#                 k = 1 + length - k
+#                 length = half
+#             else:
+#                 return "1"
 
-        def helper(length, k):
+        def helper(length, k, invert):
             if length == 1:
-                return "0"
+                return "0" if not invert else "1"
             
             half = length // 2
             # 15 -> 7 , 8
             if k <= half:
-                return helper(half, k)
+                return helper(half, k, invert)
             elif k > half + 1:
-                res = helper(half, 1 + (length - k))
-                return "0" if res == "1" else "1"
+                return helper(half, 1 + (length - k), not invert)
             else:
-                return "1"
+                return "1" if not invert else "0"
         
-        return helper(length, k)
+        return helper(length, k, False)
