50e18f2cc51d62bdb44a3e3c597690d715c33c04
Akshit Chandora <91782956+axitchandora@users.noreply.github.com>
2024-10-25 22:43:35 +0530

    Time: 159 ms (68.62%), Space: 42.7 MB (12.76%) - LeetHub

--- 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.py
+++ 1233-remove-sub-folders-from-the-filesystem/1233-remove-sub-folders-from-the-filesystem.py
@@ -1,14 +1,34 @@
+class Trie:
+    def __init__(self):
+        self.children = {} # string -> Trie
+        self.end_of_folder = False
+        
+    def add(self, path):
+        cur = self
+        for f in path.split("/"):
+            if f not in cur.children:
+                cur.children[f] = Trie()
+            cur = cur.children[f]
+        cur.end_of_folder = True
+        
+    def prefix_search(self, path):
+        cur = self
+        folders = path.split("/")
+        for i in range(len(folders) - 1):
+            cur = cur.children[folders[i]]
+            if cur.end_of_folder:
+                return True
+        return False
+
 class Solution:
     def removeSubfolders(self, folder: List[str]) -> List[str]:
-        folder_set = set(folder)
-        res = []
+        trie = Trie()
         
-        for f in folder:            
-            res.append(f)
-            for i in range(len(f)):
-                
-                if f[i] == "/" and f[:i] in folder_set:
-                    res.pop()
-                    break
+        for f in folder:
+            trie.add(f)
             
+        res = []
+        for f in folder:
+            if not trie.prefix_search(f):
+                res.append(f)
         return res
