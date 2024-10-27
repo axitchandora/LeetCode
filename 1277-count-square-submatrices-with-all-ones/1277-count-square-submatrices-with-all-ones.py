e160665d8dd8d845801bd03f1462e4ecd45ed1d0
Akshit Chandora <91782956+axitchandora@users.noreply.github.com>
2024-10-27 19:30:07 +0530

    Time: 131 ms (23.19%), Space: 28.4 MB (6.10%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.py
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.py
@@ -1,26 +1,17 @@
 class Solution:
     def countSquares(self, matrix: List[List[int]]) -> int:
         ROWS, COLS = len(matrix), len(matrix[0])
-        cache = {}
+        dp = defaultdict(int)
         
-        def dfs(r,c):
-            if r == ROWS or c == COLS or not matrix[r][c]:
-                return 0
-            
-            if (r, c) in cache:
-                return cache[(r,c)]
-            
-            cache[(r,c)] = 1 + min(
-                dfs(r + 1, c),
-                dfs(r, c + 1),
-                dfs(r + 1, c + 1)
-            )
-            
-            return cache[(r,c)]
-            
         res = 0
+        
         for r in range(ROWS):
             for c in range(COLS):
-                res += dfs(r, c)
-        
+                if matrix[r][c]:
+                    dp[(r,c)] = 1 + min(
+                        dp[(r - 1,c)],
+                        dp[(r,c - 1)],
+                        dp[(r - 1,c - 1)]
+                    )
+                    res += dp[(r,c)]
         return res
