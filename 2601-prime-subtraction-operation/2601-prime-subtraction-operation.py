8925b5fb5877b5682fbe120c5749326b2d502323
Akshit Chandora <91782956+axitchandora@users.noreply.github.com>
2024-11-11 22:41:23 +0530

    Time: 114 ms (23.46%), Space: 16.8 MB (66.41%) - LeetHub

--- 2601-prime-subtraction-operation/2601-prime-subtraction-operation.py
+++ 2601-prime-subtraction-operation/2601-prime-subtraction-operation.py
@@ -6,22 +6,18 @@ class Solution:
                     return False
             return True
         
-        primes = [False, False] # primes[i] == True, if i == prime
+        primes = [0, 0] # primes[i] == True, if i == prime
         for i in range(2, max(nums)):
             if is_prime(i):
-                primes.append(True)
+                primes.append(i)
             else:
-                primes.append(False)
+                primes.append(primes[i-1])
         
         prev = 0
         for n in nums:
             upper_bound = n - prev # non inclusive
             
-            largest_p = 0
-            for i in reversed(range(2, upper_bound)):
-                if primes[i]:
-                    largest_p = i
-                    break
+            largest_p = primes[upper_bound - 1]
             
             if n - largest_p <= prev:
                 return False
