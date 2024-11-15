dd0d4a64a478b6b37f1a98c9a55880c7f3929839
Akshit Chandora <91782956+axitchandora@users.noreply.github.com>
2024-11-15 22:24:57 +0530

    Time: 11 ms (87.39%), Space: 30.2 MB (94.70%) - LeetHub

--- 1574-shortest-subarray-to-be-removed-to-make-array-sorted/1574-shortest-subarray-to-be-removed-to-make-array-sorted.py
+++ 1574-shortest-subarray-to-be-removed-to-make-array-sorted/1574-shortest-subarray-to-be-removed-to-make-array-sorted.py
@@ -6,26 +6,15 @@ class Solution:
         while r > 0 and arr[r - 1] <= arr[r]:
             r -= 1
             
-        res = r
-        
-        # Remove postfix
-        l = 0
-        while l + 1 < N and arr[l] <= arr[l + 1]:
-            l += 1
-        res = min(res, N - 1 - l)
-        
+        res = r        
+       
         # Remove middle
-        l, r = 0, N - 1
+        l = 0
         
-        while l < r:
-            # Shrink valid window
-            while r < N and l + 1 < r and arr[r - 1] <= arr[r] and arr[l] <= arr[r]:
-                r -= 1
-                
+        while l < r:                
             # Expand Invalid Window
             while r < N and arr[l] > arr[r]:
-                r += 1
-                
+                r += 1                
             res = min(res, r - l - 1)
             if arr[l] > arr[l + 1]:
                 break
