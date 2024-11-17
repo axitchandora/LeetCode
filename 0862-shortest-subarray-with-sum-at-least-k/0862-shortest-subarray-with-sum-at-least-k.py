07975bebe8fdf7eb95e14502604d9387208dbdbc
Akshit Chandora <91782956+axitchandora@users.noreply.github.com>
2024-11-17 23:35:52 +0530

    Time: 228 ms (33.90%), Space: 31.8 MB (44.84%) - LeetHub

--- 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.py
+++ 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.py
@@ -3,7 +3,7 @@ class Solution:
         res = float("inf")
         
         cur_sum = 0
-        min_heap = [] #(prefix_sum, end_idx)
+        q = deque() #(prefix_sum, end_idx)
         
         for r in range(len(nums)):
             cur_sum += nums[r]
@@ -11,10 +11,14 @@ class Solution:
             if cur_sum >= k:
                 res = min(res, r + 1)
                 
-            # Find the min window ending at r
-            while min_heap and cur_sum - min_heap[0][0] >= k:
-                prefix, end_idx = heapq.heappop(min_heap)
+            # Find the minimum valid window ending at r
+            while q and cur_sum - q[0][0] >= k:
+                prefix, end_idx = q.popleft()
                 res = min(res, r - end_idx)
-            heapq.heappush(min_heap, (cur_sum, r))
+                
+            # Validate the monotonic deque
+            while q and q[-1][0] > cur_sum:
+                q.pop()
+            q.append((cur_sum,r))
         
         return -1 if res == float("inf") else res
