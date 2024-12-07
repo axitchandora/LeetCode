ad7e617db4d028f35b79b35d1e5d6eb53623185f
Akshit Chandora <91782956+axitchandora@users.noreply.github.com>
2024-12-07 19:35:38 +0530

    Time: 735 ms (44.21%), Space: 29.4 MB (19.09%) - LeetHub

--- 1760-minimum-limit-of-balls-in-a-bag/1760-minimum-limit-of-balls-in-a-bag.py
+++ 1760-minimum-limit-of-balls-in-a-bag/1760-minimum-limit-of-balls-in-a-bag.py
@@ -9,11 +9,13 @@ class Solution:
                     return False
             return True
         l, r = 1, max(nums)
+        res = r
         while l < r:
             m = l + ((r - l) //2)
             if can_divide(m):
                 r = m
+                res = r
             else:
                 l = m + 1
-        return l
+        return res
         
