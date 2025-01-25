946bc260236209bf8235c1ba5013d29890df04de
Akshit Chandora <91782956+axitchandora@users.noreply.github.com>
2025-01-25 22:12:35 +0530

    Add submission 1520188859

---
+++ submissions/Make Lexicographically Smallest Array by Swapping Elements/1520188859.java
@@ -0,0 +1,48 @@
+# Title: Make Lexicographically Smallest Array by Swapping Elements
+# Submission ID: 1520188859
+# Status: Accepted
+# Date: January 25, 2025 at 10:12:27 PM GMT+5:30
+
+class Solution {
+  public int[] lexicographicallySmallestArray(int[] nums, int limit) {
+    int[] ans = new int[nums.length];
+    List<List<Pair<Integer, Integer>>> numAndIndexesGroups = new ArrayList<>();
+
+    for (Pair<Integer, Integer> numAndIndex : getNumAndIndexes(nums))
+      if (numAndIndexesGroups.isEmpty() ||
+          numAndIndex.getKey() -
+                  numAndIndexesGroups.get(numAndIndexesGroups.size() - 1)
+                      .get(numAndIndexesGroups.get(numAndIndexesGroups.size() - 1).size() - 1)
+                      .getKey() >
+              limit) {
+        // Start a new group.
+        numAndIndexesGroups.add(new ArrayList<>(List.of(numAndIndex)));
+      } else {
+        // Append to the existing group.
+        numAndIndexesGroups.get(numAndIndexesGroups.size() - 1).add(numAndIndex);
+      }
+
+    for (List<Pair<Integer, Integer>> numAndIndexesGroup : numAndIndexesGroups) {
+      List<Integer> sortedNums = new ArrayList<>();
+      List<Integer> sortedIndices = new ArrayList<>();
+      for (Pair<Integer, Integer> pair : numAndIndexesGroup) {
+        sortedNums.add(pair.getKey());
+        sortedIndices.add(pair.getValue());
+      }
+      sortedIndices.sort(null);
+      for (int i = 0; i < sortedNums.size(); ++i) {
+        ans[sortedIndices.get(i)] = sortedNums.get(i);
+      }
+    }
+
+    return ans;
+  }
+
+  private Pair<Integer, Integer>[] getNumAndIndexes(int[] nums) {
+    Pair<Integer, Integer>[] numAndIndexes = new Pair[nums.length];
+    for (int i = 0; i < nums.length; ++i)
+      numAndIndexes[i] = new Pair<>(nums[i], i);
+    Arrays.sort(numAndIndexes, (a, b) -> a.getKey().compareTo(b.getKey()));
+    return numAndIndexes;
+  }
+}
