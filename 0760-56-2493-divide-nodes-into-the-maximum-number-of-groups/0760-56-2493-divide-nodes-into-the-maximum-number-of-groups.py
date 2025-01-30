f42e18ae09f0ee726b7937aed3c09279d170e61c
Akshit Chandora <91782956+axitchandora@users.noreply.github.com>
2025-01-30 22:41:34 +0530

    Time: 3077 ms, Memory: 20.1 MB - LeetHub

--- 0760-56-2493-divide-nodes-into-the-maximum-number-of-groups/0760-56-2493-divide-nodes-into-the-maximum-number-of-groups.py
+++ 0760-56-2493-divide-nodes-into-the-maximum-number-of-groups/0760-56-2493-divide-nodes-into-the-maximum-number-of-groups.py
@@ -4,20 +4,7 @@ class Solution:
         for n1, n2 in edges:
             adj[n1].append(n2)
             adj[n2].append(n1)
-
-        def get_connected_component(src):
-            q = deque([src])
-            component = set([src])
-            while q:
-                node = q.popleft()
-                for nei in adj[node]:
-                    if nei in component:
-                        continue
-                    q.append(nei)
-                    component.add(nei)
-                    visit.add(nei)
-            return component
-        
+       
         def longest_path(src):
             q = deque([(src, 1)]) # (node, group)
             dist = {src:1} # node -> length from src + 1
@@ -27,11 +14,12 @@ class Solution:
                 for nei in adj[node]:
                     if nei in dist:
                         if dist[nei] not in (length+1, length-1): #if dist[nei] == length:
-                            return -1
+                            return None, -1
                         continue
                     q.append((nei, length + 1))
                     dist[nei] = length + 1
-            return max(dist.values())
+                    visit.add(nei)
+            return dist, max(dist.values())
 
         visit = set()
         res = 0
@@ -40,13 +28,13 @@ class Solution:
                 continue
 
             visit.add(i)
-            component = get_connected_component(i)
+            component, length = longest_path(i)
+            if length == -1:
+                return -1
 
             max_cnt = 0
             for src in component:
-                length = longest_path(src)
-                if length == -1:
-                    return -1
+                _,length = longest_path(src)                
                 max_cnt = max(max_cnt, length)
             res += max_cnt
         return res
