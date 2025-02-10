fc9065dc4002f61682e82b09efc5f025e992cde1
Akshit Chandora <91782956+axitchandora@users.noreply.github.com>
2025-02-10 22:40:49 +0530

    Time: 0 ms, Memory: 17.9 MB - LeetHub

--- 0492-17-3174-clear-digits/0492-17-3174-clear-digits.py
+++ 0492-17-3174-clear-digits/0492-17-3174-clear-digits.py
@@ -2,9 +2,12 @@ class Solution:
     def clearDigits(self, s: str) -> str:
         res = []
         delete_cnt = 0
+        
+        def isdigit(c):
+            return ord("0") <= ord(c) <= ord("9")
 
         for i in reversed(range(len(s))):
-            if s[i].isdigit():
+            if isdigit(s[i]):
                 delete_cnt += 1
             elif delete_cnt:
                 delete_cnt -= 1
